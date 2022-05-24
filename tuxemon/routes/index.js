e03e5256f9df1822c33f2729f16fdcaa2fbcffbd
William Edwards <shadowapex@gmail.com>
2022-05-23 23:25:52 -0700

    Remove music section

--- tuxemon/routes/index.js
+++ tuxemon/routes/index.js
@@ -1,61 +1,87 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pages/index', {title: 'Tuxemon', page_name: 'index'});
+router.get("/", function (req, res, next) {
+  res.render("pages/index", { title: "Tuxemon", page_name: "index" });
 });
 
-router.get('/about.html', function(req, res) {
-  res.render('pages/about', {title: 'Tuxemon | About', page_name: 'about'});
+router.get("/about.html", function (req, res) {
+  res.render("pages/about", { title: "Tuxemon | About", page_name: "about" });
 });
 
-router.get('/irc.html', function(req, res) {
-  res.render('pages/irc', {title: 'Tuxemon | IRC', page_name: 'irc'});
+router.get("/irc.html", function (req, res) {
+  res.render("pages/irc", { title: "Tuxemon | IRC", page_name: "irc" });
 });
 
-router.get('/download.html', function(req, res) {
-  res.render('pages/download', {title: 'Tuxemon | Download', page_name: 'download'});
+router.get("/download.html", function (req, res) {
+  res.render("pages/download", {
+    title: "Tuxemon | Download",
+    page_name: "download",
+  });
 });
 
-router.get('/music.html', function(req, res) {
-  res.render('pages/music', {title: 'Tuxemon | Music', page_name: 'music'});
+router.get("/windows-install.html", function (req, res) {
+  res.render("pages/windows-install", {
+    title: "Tuxemon | Windows Install",
+    page_name: "windows",
+  });
 });
 
-router.get('/windows-install.html', function(req, res) {
-  res.render('pages/windows-install', {title: 'Tuxemon | Windows Install', page_name: 'windows'});
+router.get("/linux-install.html", function (req, res) {
+  res.render("pages/linux-install", {
+    title: "Tuxemon | Linux Install",
+    page_name: "linux",
+  });
 });
 
-router.get('/linux-install.html', function(req, res) {
-  res.render('pages/linux-install', {title: 'Tuxemon | Linux Install', page_name: 'linux'});
+router.get("/ubuntu-install.html", function (req, res) {
+  res.render("pages/ubuntu-install", {
+    title: "Tuxemon | Ubuntu Install",
+    page_name: "linux",
+  });
 });
 
-router.get('/ubuntu-install.html', function(req, res) {
-  res.render('pages/ubuntu-install', {title: 'Tuxemon | Ubuntu Install', page_name: 'linux'});
+router.get("/fedora-install.html", function (req, res) {
+  res.render("pages/fedora-install", {
+    title: "Tuxemon | Fedora Install",
+    page_name: "linux",
+  });
 });
 
-router.get('/fedora-install.html', function(req, res) {
-  res.render('pages/fedora-install', {title: 'Tuxemon | Fedora Install', page_name: 'linux'});
+router.get("/arch-install.html", function (req, res) {
+  res.render("pages/arch-install", {
+    title: "Tuxemon | Arch Linux Install",
+    page_name: "linux",
+  });
 });
 
-router.get('/arch-install.html', function(req, res) {
-  res.render('pages/arch-install', {title: 'Tuxemon | Arch Linux Install', page_name: 'linux'});
+router.get("/mac-install.html", function (req, res) {
+  res.render("pages/mac-install", {
+    title: "Tuxemon | Mac OS X Install",
+    page_name: "mac",
+  });
 });
 
-router.get('/mac-install.html', function(req, res) {
-  res.render('pages/mac-install', {title: 'Tuxemon | Mac OS X Install', page_name: 'mac'});
+router.get("/android-install.html", function (req, res) {
+  res.render("pages/android-install", {
+    title: "Tuxemon | Android Install",
+    page_name: "android",
+  });
 });
 
-router.get('/android-install.html', function(req, res) {
-  res.render('pages/android-install', {title: 'Tuxemon | Android Install', page_name: 'android'});
+router.get("/gcwzero-install.html", function (req, res) {
+  res.render("pages/gcwzero-install", {
+    title: "Tuxemon | GCW Zero Install",
+    page_name: "gcw",
+  });
 });
 
-router.get('/gcwzero-install.html', function(req, res) {
-  res.render('pages/gcwzero-install', {title: 'Tuxemon | GCW Zero Install', page_name: 'gcw'});
-});
-
-router.get('/donate.html', function(req, res) {
-  res.render('pages/donate', {title: 'Tuxemon | Donations', page_name: 'donate'});
+router.get("/donate.html", function (req, res) {
+  res.render("pages/donate", {
+    title: "Tuxemon | Donations",
+    page_name: "donate",
+  });
 });
 
 module.exports = router;
